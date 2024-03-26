b920763e2827a19ae845ab40e35d542c4c2e36f2
Jhayjay00 <davjiy@gmail.com>
2024-03-26 10:02:17 -0600

    My Commit

--- app.js
+++ app.js
@@ -2,40 +2,27 @@ const express = require('express');
 const ejs = require ('ejs');
 const fs = require('fs').promises;
 require('dotenv').config();
+const userRoute = require('./routes/users.js');
 const app = express();
 
-app.get('/', async (req, res) =>{
 
-try {
+app.use(userRoute);
+app.use(express.static('public'));
 
-const fileName = "example.txt";
-const content = "Zanka no tachi";
-
-//write to the file 
-await fs.writeFile(fileName, content, 'utf-8');
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
-    const data = await fs.readFile('example.txt', 'utf-8');
-
-    res.send(data);
-    console.log(data);
-    
-
-} catch (error) {
-    console.error('error reading file:', error);
-    //res.status(500).send('Internal Server Error');
-    res.send(error);
-
-}
 
 
-});
+
 
 
 
 
 
 const port =  process.env.PORT || 3000;
+
 
 app.listen(port, () =>{
 console.log(`listening on port ${port}`);
