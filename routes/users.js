b22322fb385f47b0a44cdf77e81172d4d88dd2b1
Jhayjay00 <davjiy@gmail.com>
2024-03-27 12:07:09 -0600

    new commit

--- routes/users.js
+++ routes/users.js
@@ -71,8 +71,29 @@ router.post('/users', async (req, res) =>{
 });
 
 
+router.post('/users/:id/update', async (req, res) => {
 
+    try{
+        const data = await readData();
+        const user = data.users.find(user => user.id == req.params.id);
 
+        if(user){
+            user.username = req.body.new_username || user.username;
+            user.first_name = req.body.new_first_name || user.first_name;
+            user.email = req.body.new_email || user.email;
+
+            await writeData(data);
+
+            res.send(200).send('User successfully updated.  Updated database:' + JSON.stringify(data.users));
+        } else {
+
+            res.status(404).send("User is not found.");
+        }
+
+    }catch(error){
+        res.status(500).send("Internal Server Error", error);
+    }
+});
 
 
 
